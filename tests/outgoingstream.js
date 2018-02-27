abd06b920013ab72e38b9432b05681c56ed5160b
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2018-02-27 11:40:53 +0100

    Support multiple outgoingstream video track creation

--- tests/outgoingstream.js
+++ tests/outgoingstream.js
@@ -3,6 +3,7 @@ const MediaServer	= require("../index");
 const SemanticSDP	= require("semantic-sdp");
 
 MediaServer.enableLog(false);
+MediaServer.enableDebug(false);
 const endpoint = MediaServer.createEndpoint("127.0.0.1");
 
 
@@ -46,6 +47,100 @@ tap.test("OutgoingMediaStream::create",async function(suite){
 		test.ok(outgoingStream);
 		test.same(0,outgoingStream.getAudioTracks().length);
 		test.same(1,outgoingStream.getVideoTracks().length);
+	});
+	
+	suite.test("audio+video info",async function(test){
+		//Init test
+		const transport = endpoint.createTransport({
+			dtls : SemanticSDP.DTLSInfo.expand({
+				"hash"        : "sha-256",
+				"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+			}),
+			ice  : SemanticSDP.ICEInfo.generate()
+		});
+
+		//Create new local stream
+		const outgoingStream  = transport.createOutgoingStream({
+			audio: {
+				id: "testaudio",
+				ssrcs : { media: 1},
+			},
+			video: {
+				id: "testvideo",
+				ssrcs : { media: 2, rtx: 3, fec: 4}
+			}
+			
+		});
+		test.ok(outgoingStream);
+		test.same(1,outgoingStream.getAudioTracks().length);
+		test.same(1,outgoingStream.getVideoTracks().length);
+		test.same("testaudio",outgoingStream.getAudioTracks()[0].getId());
+		test.same("testvideo",outgoingStream.getVideoTracks()[0].getId());
+	});
+	
+	suite.test("audio+video duplicated ssrc fail",async function(test){
+		//Init test
+		const transport = endpoint.createTransport({
+			dtls : SemanticSDP.DTLSInfo.expand({
+				"hash"        : "sha-256",
+				"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+			}),
+			ice  : SemanticSDP.ICEInfo.generate()
+		});
+
+		try {
+			//Create new local stream
+			const outgoingStream  = transport.createOutgoingStream({
+				audio: {
+					id: "testaudio",
+					ssrcs : { media: 1},
+				},
+				video: {
+					id: "testvideo",
+					ssrcs : { media: 1, rtx: 3, fec: 4}
+				}
+
+			});
+			test.noOk(outgoingStream);
+		} catch(error) {
+			test.ok(error);
+		}
+	});
+	
+	suite.test("audio+video+thumb info",async function(test){
+		//Init test
+		const transport = endpoint.createTransport({
+			dtls : SemanticSDP.DTLSInfo.expand({
+				"hash"        : "sha-256",
+				"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+			}),
+			ice  : SemanticSDP.ICEInfo.generate()
+		});
+
+		//Create new local stream
+		const outgoingStream  = transport.createOutgoingStream({
+			audio: {
+				id: "testaudio",
+				ssrcs : { media: 1}
+			},
+			video: [
+				{
+					id: "testvideo",
+					ssrcs : { media: 2, rtx: 3, fec: 4}
+				},
+				{
+					id: "thumb",
+					ssrcs : { media: 5, rtx: 6, fec: 7}
+				},
+			]
+			
+		});
+		test.ok(outgoingStream);
+		test.same(1,outgoingStream.getAudioTracks().length);
+		test.same(2,outgoingStream.getVideoTracks().length);
+		test.same("testaudio",outgoingStream.getAudioTracks()[0].getId());
+		test.same("testvideo",outgoingStream.getVideoTracks()[0].getId());
+		test.same("thumb",outgoingStream.getVideoTracks()[1].getId());
 	});
 	
 	suite.end();
