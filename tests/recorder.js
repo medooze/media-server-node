5b7efa4dfbe64be6dd95bc6596451e783d770171
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2020-05-06 03:10:44 +0200

    Add recorder tests

---
+++ tests/recorder.js
@@ -0,0 +1,231 @@
+const tap		= require("tap");
+const FileSystem	= require("fs");
+const Path		= require("path");
+const OS		= require("os");
+const MediaServer	= require("../index");
+const SemanticSDP	= require("semantic-sdp");
+
+MediaServer.enableLog(false);
+MediaServer.enableDebug(false);
+MediaServer.enableUltraDebug(false);
+const endpoint = MediaServer.createEndpoint("127.0.0.1");
+
+const StreamInfo	= SemanticSDP.StreamInfo;
+const TrackInfo		= SemanticSDP.TrackInfo;
+const Setup		= SemanticSDP.Setup;
+const Direction		= SemanticSDP.Direction;
+const SourceGroupInfo   = SemanticSDP.SourceGroupInfo;
+const CodecInfo		= SemanticSDP.CodecInfo;
+const TrackEncodingInfo = SemanticSDP.TrackEncodingInfo;
+
+const tmp = FileSystem.mkdtempSync(Path.join(OS.tmpdir(), 'tap-'));
+
+Promise.all([
+	tap.test("Recorder",async function(suite){
+
+		suite.test("create+stop",async function(test){
+			test.plan(2);
+			//Get temp file
+			const file = Path.join(tmp,"test1.mp4");
+			//Create 
+			const recorder = MediaServer.createRecorder(file);
+			//Get event
+			recorder.once("stopped",(that)=>{
+				//Done
+				test.same(recorder,that);
+			});
+			
+			//Check file exist
+			test.ok(FileSystem.existsSync(file));
+			
+			//Stop
+			await recorder.stop();
+			
+			//Delete it
+			FileSystem.unlinkSync(file);
+		});
+
+		suite.test("record",async function(test){
+			
+			//Init test
+			const transport = endpoint.createTransport({
+				dtls : SemanticSDP.DTLSInfo.expand({
+					"hash"        : "sha-256",
+					"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+				}),
+				ice  : SemanticSDP.ICEInfo.generate()
+			});
+
+			let ssrc = 100;
+
+			//Create stream
+			const streamInfo = new StreamInfo("stream0");
+			//Create track
+			let track = new TrackInfo("video", "track1");
+			//Get ssrc, rtx and fec 
+			const media = ssrc++;
+			const rtx = ssrc++;
+			const fec = ssrc++;
+			//Add ssrcs to track
+			track.addSSRC(media);
+			track.addSSRC(rtx);
+			track.addSSRC(fec);
+			//Add RTX and FEC group	
+			track.addSourceGroup(new SourceGroupInfo("FID",[media,rtx]));
+			track.addSourceGroup(new SourceGroupInfo("FEC-FR",[media,fec]));
+			//Add it
+			streamInfo.addTrack(track);
+			//Create track
+			track = new TrackInfo("audio", "track2");
+			//Add ssrc
+			track.addSSRC(ssrc++);
+			//Add it
+			streamInfo.addTrack(track);
+			//Create new incoming stream
+			const incomingStream = transport.createIncomingStream(streamInfo);
+			
+			//Get temp file
+			const file = Path.join(tmp,"test2.mp4");
+			//Create 
+			const recorder = MediaServer.createRecorder(file);
+			//Record it
+			recorder.record(incomingStream);
+			
+			//Check file exist
+			test.ok(FileSystem.existsSync(file));
+			
+			//Stop it
+			await recorder.stop();
+			
+			//Delete it
+			FileSystem.unlinkSync(file);
+		});
+		
+		suite.test("record+timeshift",async function(test){
+			
+			//Init test
+			const transport = endpoint.createTransport({
+				dtls : SemanticSDP.DTLSInfo.expand({
+					"hash"        : "sha-256",
+					"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+				}),
+				ice  : SemanticSDP.ICEInfo.generate()
+			});
+
+			let ssrc = 100;
+
+			//Create stream
+			const streamInfo = new StreamInfo("stream0");
+			//Create track
+			let track = new TrackInfo("video", "track1");
+			//Get ssrc, rtx and fec 
+			const media = ssrc++;
+			const rtx = ssrc++;
+			const fec = ssrc++;
+			//Add ssrcs to track
+			track.addSSRC(media);
+			track.addSSRC(rtx);
+			track.addSSRC(fec);
+			//Add RTX and FEC group	
+			track.addSourceGroup(new SourceGroupInfo("FID",[media,rtx]));
+			track.addSourceGroup(new SourceGroupInfo("FEC-FR",[media,fec]));
+			//Add it
+			streamInfo.addTrack(track);
+			//Create track
+			track = new TrackInfo("audio", "track2");
+			//Add ssrc
+			track.addSSRC(ssrc++);
+			//Add it
+			streamInfo.addTrack(track);
+			//Create new incoming stream
+			const incomingStream = transport.createIncomingStream(streamInfo);
+			
+			//Get temp file
+			const file = Path.join(tmp,"test2.mp4");
+			//Create 
+			const recorder = MediaServer.createRecorder(file,{timeShift:10000});
+			//Record it
+			recorder.record(incomingStream);
+			//Check file doesn't exist
+			test.notOk(FileSystem.existsSync(file));
+			//Flush it
+			recorder.flush();
+			//Check file exist
+			test.ok(FileSystem.existsSync(file));
+			
+			//Stop it
+			await recorder.stop();
+			
+			//Delete it
+			FileSystem.unlinkSync(file);
+		});
+		
+		suite.test("h264ParameterSet",async function(test){
+			//Init test
+			const transport = endpoint.createTransport({
+				dtls : SemanticSDP.DTLSInfo.expand({
+					"hash"        : "sha-256",
+					"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+				}),
+				ice  : SemanticSDP.ICEInfo.generate()
+			});
+
+			let ssrc = 100;
+
+			//Create stream
+			const streamInfo = new StreamInfo("stream0");
+			//Create track
+			let track = new TrackInfo("video", "track3");
+			//Get ssrc, rtx and fec 
+			const media = ssrc++;
+			const rtx = ssrc++;
+			const fec = ssrc++;
+			//Add ssrcs to track
+			track.addSSRC(media);
+			track.addSSRC(rtx);
+			track.addSSRC(fec);
+			//Add RTX and FEC group	
+			track.addSourceGroup(new SourceGroupInfo("FID",[media,rtx]));
+			track.addSourceGroup(new SourceGroupInfo("FEC-FR",[media,fec]));
+			//Add it
+			streamInfo.addTrack(track);
+			//Create track
+			track = new TrackInfo("audio", "track4");
+			//Add ssrc
+			track.addSSRC(ssrc++);
+			//Add it
+			streamInfo.addTrack(track);
+			//Create new incoming stream
+			const incomingStream = transport.createIncomingStream(streamInfo);
+			//Get video track
+			const incomingStreamTrack = incomingStream.getVideoTracks()[0];
+			//Set our of band params
+			incomingStreamTrack.setH264ParameterSets("J0LgDZWWCgPZ,KM4Ecg==");
+			//Get temp file
+			const file = Path.join(tmp,"test3.mp4");
+			//Create 
+			const recorder = MediaServer.createRecorder(file,{timeShift:10000});
+			//Record it
+			recorder.record(incomingStreamTrack);
+			//Check file doesn't exist
+			test.notOk(FileSystem.existsSync(file));
+			//Flush it
+			recorder.flush();
+			//Check file exist
+			test.ok(FileSystem.existsSync(file));
+			
+			//Stop it
+			await recorder.stop();
+			
+			//Delete it
+			FileSystem.unlinkSync(file);
+		});
+
+		suite.end();
+	})
+])
+.then(()=>{
+	MediaServer.terminate ();
+	FileSystem.rmdirSync(tmp);
+});
+
