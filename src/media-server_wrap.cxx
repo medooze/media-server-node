7eed8b5099ed1f5cd8d430b5c9e914af5e5b308e
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2022-11-24 13:59:49 +0100

    add bwe to the ontargetbitrate event

--- src/media-server_wrap.cxx
+++ src/media-server_wrap.cxx
@@ -2017,15 +2017,16 @@ public:
 		persistent = std::make_shared<Persistent<v8::Object>>(object);
 	}
 	
-	virtual void onTargetBitrateRequested(DWORD bitrate) override 
+	virtual void onTargetBitrateRequested(DWORD bitrate, DWORD bandwidthEstimation) override 
 	{
 		//Run function on main node thread
 		MediaServer::Async([=,cloned=persistent](){
 			Nan::HandleScope scope;
 			int i = 0;
-			v8::Local<v8::Value> argv[1];
+			v8::Local<v8::Value> argv[2];
 			//Create local args
 			argv[i++] = Nan::New<v8::Uint32>(bitrate);
+			argv[i++] = Nan::New<v8::Uint32>(bandwidthEstimation);
 			//Call object method with arguments
 			MakeCallback(cloned, "ontargetbitrate", i, argv);
 		
